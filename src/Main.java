0d7559a8204f301d64a0de4ceeae7b48e2edbaa8
Denis Timofeev <y719xy@gmail.com>
2020-03-19 16:15:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long amountInKopecks = 1000000;
+        long percentInKopecks = 2000;
+        long bonus = amountInKopecks / percentInKopecks;
+        System.out.println("Alexandra, hello! Thank you for choosing our airline for your flight. Your bonus is:"+bonus+" miles");
+    }
+}
